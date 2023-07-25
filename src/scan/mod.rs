83a361770987ebaa7046567cbc46ee8b57ad1f07
Luca Georges Francois <luca@quartz.technology>
2023-07-25 23:49:59 +0200

    feat(scan): add main execution logic for scan command
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -1,6 +1,11 @@
+mod metadata;
+mod scanner;
+
+use self::{metadata::Metadata, scanner::Registry};
 use anyhow::Context;
 use clap::Parser;
 use std::fs;
+use syn_solidity::{File, Item};
 
 #[derive(Debug, Parser)]
 pub struct Command {
@@ -13,8 +18,16 @@ impl Command {
         let file_path = &self.file_path;
         let code = fs::read_to_string(file_path)
             .with_context(|| format!("Failed to read content from {}", file_path))?;
+        let ast_root: File =
+            syn::parse_str(&code).with_context(|| "Failed to parse code into AST".to_string())?;
+        let ast_items: &[Item] = &ast_root.items;
+        let metadata = Metadata::new(file_path);
+        let scanners_registry = Registry::default();
 
-        println!("{:?}", code);
+        scanners_registry
+            .get_scanners()
+            .iter()
+            .for_each(|s| s.execute(ast_items, &metadata));
         Ok(())
     }
 }
