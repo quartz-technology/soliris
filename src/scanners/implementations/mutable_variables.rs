d0aef2e5d1606495508f3587b882458dae94baf5
Luca G.F <luca@quartz.technology>
2023-07-30 15:17:02 +0200

    feat(scan): add mutable variables scanner (#10)
    * feat(scan): implement mutable variables scanner
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    
    * docs(scan): document mutable variables methods
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    
    * docs: update README
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    
    ---------
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- src/scanners/implementations/mutable_variables.rs
+++ src/scanners/implementations/mutable_variables.rs
@@ -1,12 +1,49 @@
-use syn_solidity::Item;
-
 use crate::scanners::{memory::Metadata, Scanner};
+use syn_solidity::{Item, ItemContract, VariableAttribute, VariableDefinition};
 
 #[derive(Default)]
 pub struct MutableVariables {}
 
+impl MutableVariables {
+    /// Scans all variables from the contract while discarding the other items.
+    fn scan_contract(&self, contract: &ItemContract, metadata: &Metadata) {
+        for item in &contract.body {
+            if let Item::Variable(variable) = item {
+                self.scan_variable(variable, metadata)
+            }
+        }
+    }
+
+    /// Reports if a variable is likely to mutate.
+    fn scan_variable(&self, variable: &VariableDefinition, metadata: &Metadata) {
+        let immutable_variable_attributes = [
+            &VariableAttribute::Constant(Default::default()),
+            &VariableAttribute::Immutable(Default::default()),
+        ];
+
+        for iva in immutable_variable_attributes {
+            if variable.attributes.0.get(iva).is_some() {
+                return;
+            }
+        }
+
+        let line = variable.span().start().line;
+        let column = variable.span().start().column;
+
+        println!(
+            "{:}:{:}:{:} - Variable state can be changed",
+            metadata.file_path, line, column
+        )
+    }
+}
+
 impl Scanner for MutableVariables {
-    fn execute(&self, _ast: &[Item], _metadata: &Metadata) {
-        println!("todo!")
+    /// Scans every contract and reports variables able to mutate.
+    fn execute(&self, ast: &[Item], metadata: &Metadata) {
+        for item in ast {
+            if let Item::Contract(contract) = item {
+                self.scan_contract(contract, metadata)
+            }
+        }
     }
 }
