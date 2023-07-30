496a9c735e03684aa69bf5723335368ee886cc2b
Luca G.F <luca@quartz.technology>
2023-07-30 15:05:54 +0200

    feat(scan): add mutable functions scanner (#8)
    * build(deps): bump syn-solidity to latest version
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    
    * docs(scan): document mutable functions methods
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    
    * docs: update README
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    
    ---------
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- src/scanners/implementations/mutable_functions.rs
+++ src/scanners/implementations/mutable_functions.rs
@@ -1,12 +1,56 @@
-use syn_solidity::Item;
-
 use crate::scanners::{memory::Metadata, Scanner};
+use syn_solidity::{FunctionAttribute, Item, ItemContract, ItemFunction, Mutability};
 
 #[derive(Default)]
 pub struct MutableFunctions {}
 
+impl MutableFunctions {
+    /// Scans all functions from the contract while discarding the other items.
+    fn scan_contract(&self, contract: &ItemContract, metadata: &Metadata) {
+        for item in &contract.body {
+            if let Item::Function(function) = item {
+                self.scan_function(function, metadata)
+            }
+        }
+    }
+
+    /// Reports if a function is able to mutate the contract state.
+    fn scan_function(&self, function: &ItemFunction, metadata: &Metadata) {
+        // TODO: There is probably a cleaner way to check this.
+        if function.kind.as_str() == "modifier" {
+            return;
+        }
+
+        let immutable_function_attributes = [
+            &FunctionAttribute::Mutability(Mutability::Pure(Default::default())),
+            &FunctionAttribute::Mutability(Mutability::View(Default::default())),
+            &FunctionAttribute::Mutability(Mutability::Constant(Default::default())),
+        ];
+
+        for ifa in immutable_function_attributes {
+            if function.attributes.get(ifa).is_some() {
+                return;
+            }
+        }
+
+        let line = function.span().start().line;
+        let column = function.span().start().column;
+
+        println!(
+            "{:}:{:}:{:} - Function can mutate contract state",
+            metadata.file_path, line, column
+        )
+    }
+}
+
 impl Scanner for MutableFunctions {
-    fn execute(&self, _ast: &[Item], _metadata: &Metadata) {
-        println!("todo!")
+    /// Scans every contract and reports functions able to mutate the storage state.
+    fn execute(&self, ast: &[Item], metadata: &Metadata) {
+        for item in ast {
+            // Mutable functions are only located inside contracts.
+            if let Item::Contract(contract) = item {
+                self.scan_contract(contract, metadata)
+            }
+        }
     }
 }
